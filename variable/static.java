126ae4f9b86342f6990a5a86d36c15c8e13ba1dd
pradnyashippure <pradnyashippure362@gmail.com>
2022-09-04 19:44:35 +0530

    static file

---
+++ variable/static.java
@@ -0,0 +1,25 @@
+class Home {
+            static byte k;
+            static short name;
+            static int pincode;
+            static long mobNo;
+            static float sgpa;
+            static double percentage;
+            static boolean teacher;
+            static char middleName;
+
+            public static void main(String [] pra ){
+
+            System.out.println("The default value of byte is:" + k);
+            System.out.println("The default value of short is:" + name);
+            System.out.println("The default value of int is:" + pincode);
+            System.out.println("The default value of long is:" + mobNo);
+            System.out.println("The default value of float is:" + sgpa);
+            System.out.println("The default value of double is:" + percentage );
+            System.out.println("The default value of boolean is:" + teacher);
+            System.out.println("The default value of char is:" + middleName);
+            }
+}
+            
+   
+             
