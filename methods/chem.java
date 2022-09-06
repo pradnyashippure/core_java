88ff8f102e826bde849ec2a783e563a6cef85204
pradnyashippure <pradnyashippure362@gmail.com>
2022-09-06 11:55:51 +0530

    chem file

---
+++ methods/chem.java
@@ -0,0 +1,13 @@
+class Chemistry {
+        public static void main(String[]ar){
+       
+        String name1 = "Organic";
+        String name2 = "Thermodynamics";
+
+        System.out.println(name1.contains("Thermo"));
+        System.out.println(name2.valueOf("name2"));
+        System.out.println(name1.replace("Organic","inorganic"));
+        System.out.println(name1.equals("name2"));
+       }
+}
+      
