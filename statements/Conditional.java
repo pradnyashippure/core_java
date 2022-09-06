fbad328043f017bc415a294047f5a665200a9a63
pradnyashippure <pradnyashippure362@gmail.com>
2022-09-06 11:52:02 +0530

    Conditional file

---
+++ statements/Conditional.java
@@ -0,0 +1,10 @@
+class Pradnya {
+         public static void main(String []ar){
+               boolean switch_on = true;
+               if(switch_on == true){
+                 System.out.println("fan will get on");
+               } else {
+                 System.out.println("fan will not get on");
+                 }
+               }
+}
