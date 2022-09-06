3ec5601dc4dcada9d9e958ff5c6d7894ea59ba8f
pradnyashippure <pradnyashippure362@gmail.com>
2022-09-06 12:10:01 +0530

    CharArray file

---
+++ Array/CharArray.java
@@ -0,0 +1,34 @@
+class Marksheet {
+         public static void main(String [] ar) {
+                 char name1 = 'p';
+                 char name2 = 'r';
+                 char name3 = 'a';
+                 char name4 = 'd';
+                 char name5 = 'n';
+                 char name6 = 'y';
+                 char name7 = 'a';
+                 System.out.println(name1 +""+ name2 +""+ name3 +""+ name4 +""+ name5 +""+ name6 +""+ name7);
+          
+               byte [] rollno = { 1 , 2 , 3 , 4 , 5};
+               System.out.println( rollno[2] );
+
+               int [] marks = { 75 , 84 , 98 , 96 , 88 };
+               System.out.println( marks[0] );
+               
+               long [] seatno = { 425034 , 564328 , 345278 , 597641 , 335673 };
+               System.out.println( seatno[1] );
+               
+               float [] cgpa = { 6.5f , 5.8f , 7.2f , 8.2f , 8.5f };
+               System.out.println( cgpa[3] ); 
+               
+               char [] Name = { 'p' , 'a' , 'n' , 'n' , 'u' };
+               System.out.println( Name );
+ 
+               double [] percentage = { 68.3d , 72.1d , 70,5d , 82.4d , 85.5d };
+               System.out.println( percentage[4] );
+
+               boolean [] fail = { true , false , true , false , false };
+               System.out.println( fail[0] );
+
+               }
+}
