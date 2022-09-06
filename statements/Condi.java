b54b4e36bd0512ca1c2fc7cb65c1f68a68c31ed9
pradnyashippure <pradnyashippure362@gmail.com>
2022-09-06 11:50:19 +0530

    Condi file

---
+++ statements/Condi.java
@@ -0,0 +1,11 @@
+class Maths {
+         public static void main(String []ar){
+               int number = 5678439;
+               int no     = 1234567;
+               if(number >= no){
+                 System.out.println("above statement is true");
+               } else {
+                 System.out.println("above statement is false");
+                 }
+               }
+}
