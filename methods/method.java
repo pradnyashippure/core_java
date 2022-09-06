e7f59c07ac0f9a510ec987fff244017d9b807d93
pradnyashippure <pradnyashippure362@gmail.com>
2022-09-06 11:56:50 +0530

    method file

---
+++ methods/method.java
@@ -0,0 +1,10 @@
+class Book {
+       public static void main(String[]ar){
+       String science = "Biology" ;
+           System.out.println(science.charAt(5));
+           System.out.println(science.indexOf("l"));
+           System.out.println(science.toUpperCase());
+           System.out.println(science.toLowerCase());
+           System.out.println(science.length());
+          }
+}
