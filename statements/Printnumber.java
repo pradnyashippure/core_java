99a50be5cf3e31cce687886a82ba60b52d9e8ba3
pradnyashippure <pradnyashippure362@gmail.com>
2022-09-06 11:44:27 +0530

    Printnumber file

---
+++ statements/Printnumber.java
@@ -0,0 +1,14 @@
+class PrintNumber {
+       public static void main(String []ar){
+       byte [] rollno = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+       System.out.println("size of array"+rollno.length);
+
+       for(byte num = 0; num < rollno.length; num++){
+           if(rollno [num] % 2 == 0){
+           System.out.println(rollno [num] +"is even number");
+           }else{
+           System.out.println(rollno [num] +"is odd number");
+           }
+         }
+     }
+}
