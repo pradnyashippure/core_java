22ac7f7bcfb31498989801ab076559a52610578c
pradnyashippure <pradnyashippure362@gmail.com>
2022-09-06 11:41:31 +0530

    Details file

---
+++ Data type/Details.java
@@ -0,0 +1,12 @@
+class Pradnya {
+       public static void main(String [] pannu){
+               System.out.println("Starting of main method");
+               byte age ;
+               age = 22 ;
+               System.out.println( age );
+               char middleName = 'p';
+               System.out.println( middleName );
+               boolean ismarried = false;
+               System.out.println( ismarried );
+               }
+}
