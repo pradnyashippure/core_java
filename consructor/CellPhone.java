897fd6f133fdb8714079e27f01d734deb29c0b95
pradnyashippure <pradnyashippure362@gmail.com>
2022-09-08 10:54:22 +0530

    CellPhone file

---
+++ consructor/CellPhone.java
@@ -0,0 +1,12 @@
+class CellPhone {
+       String brandname ;
+       String modelname;
+       char color;
+       byte generation;
+       
+        CellPhone (String bname) {
+	       System.out.println("creating object");
+               brandname =  bname;
+               System.out.println("object created");
+    }
+}
