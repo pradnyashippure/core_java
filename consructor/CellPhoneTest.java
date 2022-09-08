029ec70c3dc6aabd5b3c3bc7f9949e4cf6780fb8
pradnyashippure <pradnyashippure362@gmail.com>
2022-09-08 10:56:48 +0530

    CellPhoneTest file

---
+++ consructor/CellPhoneTest.java
@@ -0,0 +1,8 @@
+class CellPhoneTest {
+        public static void main(String []args){
+
+         CellPhone  cellPhone = new CellPhone ("Samsung");
+         System.out.println(cellPhone);
+         System.out.println(cellPhone.brandname+"\t"+cellPhone.modelname+"\t"+cellPhone.color+"\t"+cellPhone.generation);
+    }
+}
